f63a662907623d962cc3f53638e15034bf5c545c
lakshayjn <72554267+lakshayjn@users.noreply.github.com>
2020-11-20 14:50:46 +0530

    Add files via upload

---
+++ ChainClass.js
@@ -0,0 +1,22 @@
+class Chain {
+    constructor(bodyA ,bodyB){
+var options = {
+    bodyA:bodyA,
+    bodyB:bodyB,
+    stiffness:0.1,
+    length:250
+}
+this.Chain = Constraint.create(options)
+World.add(world,this.Chain)
+
+ }
+display(){
+    var pointA = this.Chain.bodyA.position;
+    var pointB = this.Chain.bodyB.position;
+
+    strokeWeight(4);
+line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+}
+
+}
